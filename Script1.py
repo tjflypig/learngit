8a23d5d3538bc11124ae6055cd2455f8cc0f6b40
tjzsf <hustflypig@163.com>
2015-09-10 11:41:15 +0800

    change script1 function

--- Script1.py
+++ Script1.py
@@ -1,14 +1,27 @@
 import urllib
 url0='http://blog.sina.com.cn/u/1347712670'
 con=urllib.urlopen(url0).read()
+'''
 i=0
+htmlindex=[]
 html=[]
-htmlindex=con.find('<a href=')
+htmlindex.append(con.find('<a href='))
 htmlindex1=con.find('</a>',htmlindex)
 html.append(con[htmlindex+9:htmlindex1+4])
 while (i<10 and i!=0):
-    htmlindex=con.find('<a href=',htmlindex1)
+    htmlindex.append(con.find('<a href=',htmlindex1))
     htmlindex1=con.find('</a>',htmlindex)
     html.append(con[htmlindex+9:htmlindex1+4])
     i=i+1
 print html
+'''
+html=[]
+i=0
+htmlindex=1
+htmlindex1=0
+while (htmlindex):
+	htmlindex=con.find('<a href=',htmlindex1)
+	htmlindex1=con.find('</a>',htmlindex)
+	if con.find('http',htmlindex,htmlindex1)!= -1 and con.find('html',htmlindex,htmlindex1)!= -1:
+		html.append(con[con.find('http',htmlindex,htmlindex1):con.find('html',htmlindex,htmlindex1)+4])
+	
